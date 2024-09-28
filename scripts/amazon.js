4adfa394abd12c26d5cbe2eaf76e388a3f198701
Dickson Wachira <programmingwithdickson@gmail.com>
2024-09-28 18:12:07 +0300

    Create functions and modules to use in order summary

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,6 @@
+import {cart, addToCart} from '../data/cart.js';
+import {products} from '../data/products.js';
+
 let productsHTML = ''
 
 products.forEach((product) => {
@@ -54,31 +57,24 @@ products.forEach((product) => {
 });
 
 
-document.querySelector('.js-products-grid').
-innerHTML = productsHTML;
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+function updateCartQuantity() {
+       let cartQuantity = 0;
+
+            cart.forEach((cartItem) => {
+                cartQuantity += cartItem.quantity;
+            });
+
+            document.querySelector('.js-cart-quantity')
+             .innerHTML = cartQuantity;
+}
 
 document.querySelectorAll('.js-add-to-cart')
 .forEach((button) => {
     button.addEventListener('click', () => {
         const productId = button.dataset.productId;
-
-        let matchingItem;
-
-        cart.forEach((item) => {
-            if (productId === item.productId){
-                matchingItem = item;
-            }
-        });
-
-            if(matchingItem){
-                matchingItem.quantity += 1;
-            }else {
-                cart.push({
-                    productId: productId,
-                    quantity: 1
-                });
-            }
-
-        console.log(cart);
+        addToCart(productId);
+        updateCartQuantity();
     });
 });
