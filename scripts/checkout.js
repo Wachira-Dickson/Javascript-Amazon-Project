3785f97a9513fc7f8c6782f4a6cde746691b4011
Your Name <your_email@example.com>
2025-05-18 16:49:23 +0300

    Load the cart-class file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
-import {renderOrderSummary} from './Checkout/orderSummary.js';
-import { renderPaymentSummary } from './Checkout/paymentSummary.js';
+import {renderOrderSummary} from './checkout/orderSummary.js';
+import { renderPaymentSummary } from './checkout/paymentSummary.js'; 
+import '../data/cart-class.js';
 
 renderOrderSummary();
 renderPaymentSummary();
